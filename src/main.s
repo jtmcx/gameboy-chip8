f9a29401608ad4a0bbae24b9a05764ab247b2c35
jtm <noreply@jtm.cx>
2019-09-02 15:56:06 -0400

    Added SUB instruction

--- src/main.s
+++ src/main.s
@@ -483,6 +483,28 @@ op_add:
 	call	incpc
 	ret
 
+;; --------------------------------------------------------------------
+;; op_sub
+;; --------------------------------------------------------------------
+
+op_sub:
+	call	loadvy
+	ld	d, a
+	call	loadvx
+	sub	a, d
+	ld	[hl], a
+
+	;; Set VF approproiately
+	jr	c, .carry
+	ld	a, 1
+	jr	.end
+.carry
+	xor	a, a
+.end
+	ld	[Chip8Regs+$F], a
+	call	incpc
+	ret
+
 
 SECTION "jump tables", ROM0
 JumpTabMain:
@@ -509,7 +531,7 @@ dw	op_or	; 0x8__1
 dw	op_and	; 0x8__2
 dw	op_xor	; 0x8__3
 dw	op_add	; 0x8__4
-dw	incpc	; 0x8__5
+dw	op_sub	; 0x8__5
 dw	incpc	; 0x8__6
 dw	incpc	; 0x8__7
 dw	incpc	; 0x8__8
